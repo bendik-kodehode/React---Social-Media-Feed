d9e91e58041f0d6aa9f8c93f1a12da49a54e8fb8
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-03-19 14:18:37 +0100

    Started styling, removed content div from AddPost, using form instead.

--- src/AddPost/AddPost.jsx
+++ src/AddPost/AddPost.jsx
@@ -1,6 +1,4 @@
 import style from "./AddPost.module.css";
-import defaultPosts from "../assets/posts";
-import { useState } from "react";
 
 function AddPost({setPosts}) {
     const handleSubmit = (e) => {
@@ -16,12 +14,10 @@ function AddPost({setPosts}) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className={style.content}>
-                <input type="text" name="username" placeholder="Username" autoComplete="off" required></input>
-                <textarea name="content" placeholder="What's on your mind?" autoComplete="off" required></textarea>
-                <button type="submit">Post</button>
-            </div>
+        <form onSubmit={handleSubmit} className={style.content}>
+            <input type="text" name="username" placeholder="Username" autoComplete="off" required></input>
+            <textarea name="content" placeholder="What's on your mind?" autoComplete="off" required></textarea>
+            <button type="submit">Post</button>
         </form>
     )
 }
